aec1c2a53a13601e243e09a75bdb248d61cbdab1
iSteal-it <47010892+iSteal-it@users.noreply.github.com>
2021-08-01 20:02:40 +0530

    Update main.py

--- day-1/main.py
+++ day-1/main.py
@@ -1,1 +1,7 @@
+total_bill = float(input("What was the total bill? $"))
+people = float(input("How many people splitting the bill\n"))
+percentage = float(input("What percentage of tip you would give? 10 15 or 20?\n"))
 
+cal = (total_bill + (percentage / 100 * total_bill)) / people 
+
+print(F"Each person should pay {cal}")
