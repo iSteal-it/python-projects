3760eee90025f0708c49c01b031ba455e91cca7f
iSteal-it <47010892+iSteal-it@users.noreply.github.com>
2021-08-13 15:14:18 +0530

    Create computerai.py

---
+++ day-11 Battleship/computerai.py
@@ -0,0 +1,6 @@
+import random 
+
+class Computer_AI:
+  def __init__(self):
+    self.possible_positions = ["11","12","13","14","15","21","22","23","24","25","31","32","33","34","35","41","42","43","44","45","51","52","53","54","55"]
+    self.ship_position = []
