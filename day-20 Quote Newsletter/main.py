4fc3d9d0dc32f0fc980c72f8773f477eed0fbdbe
iSteal-it <47010892+iSteal-it@users.noreply.github.com>
2021-08-20 12:15:41 +0530

    Create main.py

---
+++ day-20 Quote Newsletter/main.py
@@ -0,0 +1,23 @@
+import smtplib
+import pandas
+import datetime
+import random
+
+now = datetime.datetime.now()
+
+now_day = now.strftime("%A")
+
+if now_day == "Friday":
+    data = pandas.read_csv("quotes.txt",header=None).to_dict(orient="records")
+    quote = random.choice(data)
+    my_email = "admin@pixelpaste.net"
+    password = "Krevory123@"
+    connection = smtplib.SMTP("smtp.hostinger.com",587)
+    connection.starttls()
+    connection.login(user=my_email,password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="bobbobpvp@gmail.com",
+        msg=f"From:Admin@pixelpaste.net\nSubject:Today Quote\n\n {quote[0]}"
+    )
+    connection.close()
