ce758f73a29ed3484e3b40360ec1dbd736eba688
iSteal-it <47010892+iSteal-it@users.noreply.github.com>
2021-09-05 21:02:20 +0530

    Update main.py

--- day-35 cookie game autoclicker bot/main.py
+++ day-35 cookie game autoclicker bot/main.py
@@ -1,6 +1,6 @@
 import time
 from selenium import webdriver
-
+from selenium.common import exceptions
 chromedriver: str = "/Users/alyadav/Desktop/chromedriver"
 driver = webdriver.Chrome(executable_path=chromedriver)
 
@@ -12,18 +12,23 @@ while True:
     for _ in range(5):
         time.sleep(1)
         cookie.click()
-    cursor = driver.find_element_by_id("buyCursor")
-    grandma = driver.find_element_by_id("buyGrandma")
-    factory = driver.find_element_by_id("buyFactory")
-    mine = driver.find_element_by_id("buyMine")
-    portal = driver.find_element_by_id("buyPortal")
-    if portal.get_attribute("class") == "":
-        portal.click()
-    elif mine.get_attribute("class") == "":
-        mine.click()
-    elif factory.get_attribute("class") == "":
-        factory.click()
-    elif grandma.get_attribute("class") == "":
-        grandma.click()
-    elif cursor.get_attribute("class") == "":
-        cursor.click()
+    try:
+        cursor = driver.find_element_by_id("buyCursor")
+        grandma = driver.find_element_by_id("buyGrandma")
+        factory = driver.find_element_by_id("buyFactory")
+        mine = driver.find_element_by_id("buyMine")
+        portal = driver.find_element_by_id("buyPortal")
+        if portal.get_attribute("class") == "":
+            portal.click()
+        elif mine.get_attribute("class") == "":
+            mine.click()
+        elif factory.get_attribute("class") == "":
+            factory.click()
+        elif grandma.get_attribute("class") == "":
+            grandma.click()
+        elif cursor.get_attribute("class") == "":
+            cursor.click()
+
+    except exceptions.StaleElementReferenceException:
+        print("e")
+        pass
