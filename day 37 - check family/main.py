43a223b58d73db85660f29380e0f2fa0943df766
iSteal-it <47010892+iSteal-it@users.noreply.github.com>
2021-09-11 18:59:52 +0530

    Update main.py

--- day 37 - check family/main.py
+++ day 37 - check family/main.py
@@ -1,16 +1,32 @@
 from selenium import webdriver
 import time
-from selenium.webdriver.common.action_chains import ActionChains
 
-
-email = "rus@isteal.me"
+email = ""
 password = ""
+
 chromedriver: str = "/Users/alyadav/Desktop/chromedriver"
 driver = webdriver.Chrome(executable_path=chromedriver)
 
 data = driver.get("https://www.icloud.com/")
+window_now = driver.window_handles[0]
+time.sleep(10)
 
-time.sleep(20)
+driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
 
-element = driver.find_element_by_class_name("cw-pane-container")
-element.click()
+email_field = driver.find_element_by_xpath('//*[@id="account_name_text_field"]')
+email_field.send_keys(email)
+
+chk = driver.find_element_by_xpath('/html/body/div[3]/apple-auth/div/div[1]/div/sign-in/div/div[1]/button[1]')
+chk.click()
+
+time.sleep(5)
+
+password_field = driver.find_element_by_id('password_text_field')
+password_field.send_keys(password)
+
+chk = driver.find_element_by_xpath('//*[@id="sign-in"]')
+chk.click()
+
+driver.switch_to.window(window_now)
+
+time.sleep(10)
