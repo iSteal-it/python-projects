a2cd0fe5a63cde251d6154d400b84ac14d1c0fd5
iSteal-it <47010892+iSteal-it@users.noreply.github.com>
2021-09-13 11:26:16 +0530

    Create  main.py

---
+++ day - 39 flask/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/bye")
+def bye():
+    return "<p>bye</p>"
+if __name__ == "__main__":
+    app.run()
