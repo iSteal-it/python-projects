5e0a2451a6776f111ddad46a06b5e60fd625b0f8
iSteal-it <47010892+iSteal-it@users.noreply.github.com>
2021-09-15 13:06:59 +0530

    Add files via upload

---
+++ day 41 card app/server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
